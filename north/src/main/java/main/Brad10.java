3b063170aabececa2d281bd18997cdad03ef1a01
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-26 16:10:36 +0800

    05261610

--- north/src/main/java/main/Brad10.java
+++ north/src/main/java/main/Brad10.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Session;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /*
  * SQL:
@@ -57,17 +62,42 @@ public class Brad10 {
 				System.out.println("Product:" + row[2] + ":" + row[3] + ":" +row[4]);
 				System.out.println("-----");
 			}
+			System.out.println("--- JSON 1 ---");
+			ObjectMapper mapper = new ObjectMapper();
+			String json =
+				mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+			System.out.println(json);
 			
+			toObjectJSON(result);
 			
 		}catch(Exception e) {
 			System.out.println(e);
 		}
+	}
+	
+	static void toObjectJSON(List<Object[]> result) throws Exception{
+		Map<String,Object> output = new HashMap<>();
+		output.put("customer", result.get(0)[1]);
+		output.put("employee", result.get(0)[0]);
 		
+		List<Map<String,Object>> details = new ArrayList<>();
+		for (Object[] row : result) {
+			Map<String,Object> detail = new HashMap<>();
+			detail.put("pname", row[2]);
+			detail.put("price", row[3]);
+			detail.put("qty", row[4]);
+			details.add(detail);
+		}
+		output.put("details", details);
 		
-		
-		
-		
+		System.out.println("--- JSON 2 ---");
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writerWithDefaultPrettyPrinter()
+				.writeValueAsString(output);
+		System.out.println(json);
 		
 	}
+	
+	
 
 }
