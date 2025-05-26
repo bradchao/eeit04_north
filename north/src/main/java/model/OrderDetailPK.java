b50bfda7c311813485a978c808d35e19e458f9cc
mickeyyo55b <mickeyyo55b@gmail.com>
2025-05-26 15:11:40 +0800

    05261511

--- north/src/main/java/model/OrderDetailPK.java
+++ north/src/main/java/model/OrderDetailPK.java
@@ -1,5 +1,25 @@
 package model;
 
-public class OrderDetailPK {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class OrderDetailPK implements Serializable{
+	private Integer orderId;
+	private Integer productId;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(orderId, productId);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof OrderDetailPK)) return false;
+		OrderDetailPK other = (OrderDetailPK)o;
+		return orderId == other.orderId && productId == other.productId;
+	}
+	
+	
 
 }
